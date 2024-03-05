48df6abd4c0153ab597916298417b790dc450b55
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-03-05 09:35:20 -0300

    Feat: Setup inicial do ms de gestao de itens

---
+++ ms-gt-itens/src/main/java/br/com/fiap/msGtItens/gtItens/GtItensApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.msGtItens.gtItens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtItensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtItensApplication.class, args);
+	}
+
+}
---
+++ ms-gt-itens/src/test/java/br/com/fiap/msGtItens/gtItens/GtItensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.msGtItens.gtItens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtItensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
