6dd0eaae8811f9b3d78a58fa91e00b5df3dff70e
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-03-05 19:10:11 -0300

    Feat: Setup inicial ms-carrinho

---
+++ ms-carrinho/src/main/java/br/com/fiap/msCarrinho/msCarrinho/MsCarrinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.msCarrinho.msCarrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCarrinhoApplication.class, args);
+	}
+
+}
---
+++ ms-carrinho/src/test/java/br/com/fiap/msCarrinho/msCarrinho/MsCarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.msCarrinho.msCarrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
