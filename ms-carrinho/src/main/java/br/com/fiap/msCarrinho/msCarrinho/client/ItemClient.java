07e6efbd90d3c080667ecaa8b83b4b79c9a7e181
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-03-22 00:04:09 -0300

    Feat: Atualizando docker-compose e docker files

--- ms-carrinho/src/main/java/br/com/fiap/msCarrinho/msCarrinho/client/ItemClient.java
+++ ms-carrinho/src/main/java/br/com/fiap/msCarrinho/msCarrinho/client/ItemClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msItem", url = "localhost:8082/ms-gt-itens/itens")
+@FeignClient(name = "msItem", url = "${ms.itens.url}")
 public interface ItemClient {
 
     @GetMapping("/{id}")
